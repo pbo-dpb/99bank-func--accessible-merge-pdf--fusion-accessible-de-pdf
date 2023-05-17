d95ece1ff2cf6cc246c7d0d41aee3cb4e624cb39
Rémy Vanherweghem <remy.vanherweghem@parl.gc.ca>
2023-05-17 11:48:52 +0000

    Removed harcoded URLs from function.

--- index.js
+++ index.js
@@ -11,23 +11,25 @@ async function readDocumentMetadata(body) {
 
 exports.handler = async function (event, context) {
 
-    const url1 = 'https://pbo-dpb--static-statique--dev.s3.ca-central-1.amazonaws.com/Models/Publication/107/coverpage_en.pdf?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAW27BPFTARAOXBXG7%2F20230517%2Fca-central-1%2Fs3%2Faws4_request&X-Amz-Date=20230517T022654Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Signature=f09f8b4c4683c36541f1cd56a2f59cbf18e9a3a37a30cb1cf54c2c1966ee3a79'
-    const url2 = 'https://vanherweghem-misc.s3-ca-central-1.amazonaws.com/tagged-pdf.pdf'
+    /**
+     * Expects a JSON object with two URLs: `cover` and `main`.
+     */
+    let payload = JSON.parse(event.body);
 
-    const firstDonorPdfBytes = await fetch(url1).then(res => res.arrayBuffer())
-    const secondDonorPdfBytes = await fetch(url2).then(res => res.arrayBuffer())
 
-    const pdfA = await PDFDocument.load(firstDonorPdfBytes)
-    const pdfB = await PDFDocument.load(secondDonorPdfBytes)
-    pdfB.removePage(0);
+    const coverBytes = await fetch(payload.cover).then(res => res.arrayBuffer())
+    const mainBytes = await fetch(payload.main).then(res => res.arrayBuffer())
 
-    const copiedPagesA = await pdfB.copyPages(pdfA, pdfA.getPageIndices());
+    const coverPdf = await PDFDocument.load(coverBytes)
+    const mainPdf = await PDFDocument.load(mainBytes)
+    mainPdf.removePage(0);
+
+    const copiedPages = await mainPdf.copyPages(coverPdf, coverPdf.getPageIndices());
     let index = 0;
-    copiedPagesA.forEach((page) => { pdfB.insertPage(0 + index, page); index++ });
+    copiedPages.forEach((page) => { mainPdf.insertPage(0 + index, page); index++ });
 
-    const mergedPdfBytes = await pdfB.save();
+    const mergedPdfBytes = await mainPdf.save();
 
-
-    fs.writeFile('./message7.pdf', Buffer.from(new Uint8Array(mergedPdfBytes)), () => { });
+    return mergedPdfBytes;
 
 }
